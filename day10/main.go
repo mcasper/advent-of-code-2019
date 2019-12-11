f606d99cec50013ef3b007e17621dbd08d1ba9f7
Matt Casper <matthewvcasper@gmail.com>
2019-12-10 22:16:01 -0800

    Day 10, part 1, golang

---
+++ day10/main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"math"
+	"strings"
+)
+
+type Asteroid struct {
+	x float64
+	y float64
+}
+
+func (a Asteroid) Total() float64 {
+	return a.x + a.y
+}
+
+func main() {
+	input, err := ioutil.ReadFile("part1.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	lines := strings.Split(strings.Trim(string(input), "\n"), "\n")
+
+	grid := [][]string{}
+	for _, line := range lines {
+		grid = append(grid, strings.Split(line, ""))
+	}
+
+	result := Execute(grid)
+	fmt.Printf("Part 1 result: %v\n", result)
+}
+
+func Execute(grid [][]string) int {
+	asteroids := []Asteroid{}
+
+	for y, row := range grid {
+		for x, space := range row {
+			if space == "#" {
+				asteroids = append(asteroids, Asteroid{float64(x), float64(y)})
+			}
+		}
+	}
+
+	mostVisibleAsteroids := 0
+
+	for _, sourceAsteroid := range asteroids {
+		visibleAsteroids := 0
+
+		for _, destAsteroid := range asteroids {
+			if sourceAsteroid == destAsteroid {
+				continue
+			}
+
+			if !anyBlockingAsteroids(sourceAsteroid, destAsteroid, asteroids) {
+				visibleAsteroids++
+			}
+		}
+
+		if mostVisibleAsteroids < visibleAsteroids {
+			mostVisibleAsteroids = visibleAsteroids
+		}
+	}
+
+	return mostVisibleAsteroids
+}
+
+func anyBlockingAsteroids(sourceAsteroid Asteroid, destAsteroid Asteroid, asteroids []Asteroid) bool {
+	for _, potentiallyBlockingAsteroid := range asteroids {
+		if sourceAsteroid == potentiallyBlockingAsteroid || destAsteroid == potentiallyBlockingAsteroid {
+			continue
+		}
+
+		if asteroidBlocksLOS(sourceAsteroid, destAsteroid, potentiallyBlockingAsteroid) {
+			return true
+		}
+	}
+
+	return false
+}
+
+func asteroidBlocksLOS(sourceAsteroid, destAsteroid, potentiallyBlockingAsteroid Asteroid) bool {
+	if angle(sourceAsteroid, destAsteroid) == angle(sourceAsteroid, potentiallyBlockingAsteroid) {
+		return distance(sourceAsteroid, potentiallyBlockingAsteroid) < distance(sourceAsteroid, destAsteroid)
+	}
+
+	return false
+}
+
+func angle(a, b Asteroid) float64 {
+	return math.Atan2(b.y-a.y, b.x-a.x) * 180 / math.Pi
+}
+
+func distance(a, b Asteroid) float64 {
+	return math.Abs(float64(a.x-b.x)) + math.Abs(float64(a.y-b.y))
+}
---
+++ day10/main_test.go
@@ -0,0 +1,173 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestExecute(t *testing.T) {
+	grid := [][]string{
+		[]string{
+			".", "#", ".", ".", "#",
+		},
+		[]string{
+			".", ".", ".", ".", ".",
+		},
+		[]string{
+			"#", "#", "#", "#", "#",
+		},
+		[]string{
+			".", ".", ".", ".", "#",
+		},
+		[]string{
+			".", ".", ".", "#", "#",
+		},
+	}
+	expected := 8
+	result := Execute(grid)
+
+	if result != expected {
+		t.Errorf("Expected %v, got %v\n", expected, result)
+	}
+
+	grid = [][]string{
+		[]string{
+			".", ".", ".", ".", ".", ".", "#", ".", "#", ".",
+		},
+		[]string{
+			"#", ".", ".", "#", ".", "#", ".", ".", ".", ".",
+		},
+		[]string{
+			".", ".", "#", "#", "#", "#", "#", "#", "#", ".",
+		},
+		[]string{
+			".", "#", ".", "#", ".", "#", "#", "#", ".", ".",
+		},
+		[]string{
+			".", "#", ".", ".", "#", ".", ".", ".", ".", ".",
+		},
+		[]string{
+			".", ".", "#", ".", ".", ".", ".", "#", ".", "#",
+		},
+		[]string{
+			"#", ".", ".", "#", ".", ".", ".", ".", "#", ".",
+		},
+		[]string{
+			".", "#", "#", ".", "#", ".", ".", "#", "#", "#",
+		},
+		[]string{
+			"#", "#", ".", ".", ".", "#", ".", ".", "#", ".",
+		},
+		[]string{
+			".", "#", ".", ".", ".", ".", "#", "#", "#", "#",
+		},
+	}
+
+	expected = 33
+	result = Execute(grid)
+
+	if result != expected {
+		t.Errorf("Expected %v, got %v\n", expected, result)
+	}
+
+	grid = [][]string{
+		[]string{
+			"#", ".", "#", ".", ".", ".", "#", ".", "#", ".",
+		},
+		[]string{
+			".", "#", "#", "#", ".", ".", ".", ".", "#", ".",
+		},
+		[]string{
+			".", "#", ".", ".", ".", ".", "#", ".", ".", ".",
+		},
+		[]string{
+			"#", "#", ".", "#", ".", "#", ".", "#", ".", "#",
+		},
+		[]string{
+			".", ".", ".", ".", "#", ".", "#", ".", "#", ".",
+		},
+		[]string{
+			".", "#", "#", ".", ".", "#", "#", "#", ".", "#",
+		},
+		[]string{
+			".", ".", "#", ".", ".", ".", "#", "#", ".", ".",
+		},
+		[]string{
+			".", ".", "#", "#", ".", ".", ".", ".", "#", "#",
+		},
+		[]string{
+			".", ".", ".", ".", ".", ".", "#", ".", ".", ".",
+		},
+		[]string{
+			".", "#", "#", "#", "#", ".", "#", "#", "#", ".",
+		},
+	}
+
+	expected = 35
+	result = Execute(grid)
+
+	if result != expected {
+		t.Errorf("Expected %v, got %v\n", expected, result)
+	}
+
+	grid = [][]string{
+		[]string{
+			".", "#", ".", ".", "#", ".", ".", "#", "#", "#",
+		},
+		[]string{
+			"#", "#", "#", "#", ".", "#", "#", "#", ".", "#",
+		},
+		[]string{
+			".", ".", ".", ".", "#", "#", "#", ".", "#", ".",
+		},
+		[]string{
+			".", ".", "#", "#", "#", ".", "#", "#", ".", "#",
+		},
+		[]string{
+			"#", "#", ".", "#", "#", ".", "#", ".", "#", ".",
+		},
+		[]string{
+			".", ".", ".", ".", "#", "#", "#", ".", ".", "#",
+		},
+		[]string{
+			".", ".", "#", ".", "#", ".", ".", "#", ".", "#",
+		},
+		[]string{
+			"#", ".", ".", "#", ".", "#", ".", "#", "#", "#",
+		},
+		[]string{
+			".", "#", "#", ".", ".", ".", "#", "#", ".", "#",
+		},
+		[]string{
+			".", ".", ".", ".", ".", "#", ".", "#", ".", ".",
+		},
+	}
+
+	expected = 41
+	result = Execute(grid)
+
+	if result != expected {
+		t.Errorf("Expected %v, got %v\n", expected, result)
+	}
+}
+
+func TestAsteroidBlocksLOS(t *testing.T) {
+	sourceAsteroid := Asteroid{x: 1, y: 0}
+	destAsteroid := Asteroid{x: 0, y: 2}
+	potentiallyBlockingAsteroid := Asteroid{x: 1, y: 2}
+	expected := false
+	result := asteroidBlocksLOS(sourceAsteroid, destAsteroid, potentiallyBlockingAsteroid)
+
+	if result != expected {
+		t.Errorf("Expected %v, got %v\n", expected, result)
+	}
+
+	sourceAsteroid = Asteroid{x: 3, y: 4}
+	destAsteroid = Asteroid{x: 1, y: 0}
+	potentiallyBlockingAsteroid = Asteroid{x: 2, y: 2}
+	expected = true
+	result = asteroidBlocksLOS(sourceAsteroid, destAsteroid, potentiallyBlockingAsteroid)
+
+	if result != expected {
+		t.Errorf("Expected %v, got %v\n", expected, result)
+	}
+}
